7dc3ffea41743863f7022fe67b71f38f6f61ac5f
Rishab <rishab.jituri@gmail.com>
2025-01-25 22:59:17 -0500

    first

---
+++ src/road.c
@@ -0,0 +1,14 @@
+/**
+ * Each lane will be a single layer list. 
+ * Size will remain immutable
+ * vehicles will traverse in the ascending direction of the list
+ * 
+ */
+
+typedef struct tile{
+    
+}
+
+typedef struct lane{
+    
+};
